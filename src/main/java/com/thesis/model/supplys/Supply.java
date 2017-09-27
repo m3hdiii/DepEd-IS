c6ea882f6ccfd6deb4f20c3cc8740c2c39507059
kevin <kevin.orian@yahoo.com>
2017-09-27 19:06:36 +0800

    mehdi's work

--- src/main/java/com/thesis/model/supplys/Supply.java
+++ src/main/java/com/thesis/model/supplys/Supply.java
@@ -12,7 +12,7 @@ import java.util.Set;
  */
 
 @Entity
-@Table(name = "supplyofficer")
+@Table(name = "supply")
 public class Supply {
 
     @Id
@@ -32,5 +32,43 @@ public class Supply {
     @OneToMany
     private Set<Item> items = new HashSet<>();
 
+    public Long getSupplyId() {
+        return supplyId;
+    }
 
+    public void setSupplyId(Long supplyId) {
+        this.supplyId = supplyId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public DeliveryInformation getDeliveryInformation() {
+        return deliveryInformation;
+    }
+
+    public void setDeliveryInformation(DeliveryInformation deliveryInformation) {
+        this.deliveryInformation = deliveryInformation;
+    }
+
+    public Set<Equipment> getEquipments() {
+        return equipments;
+    }
+
+    public void setEquipments(Set<Equipment> equipments) {
+        this.equipments = equipments;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Item> items) {
+        this.items = items;
+    }
 }
---
+++ src/main/java/com/thesis/service/supply/SupplyService.java
@@ -0,0 +1,7 @@
+package com.thesis.service.supply;
+
+import com.thesis.model.supplys.Supply;
+import com.thesis.service.BaseService;
+
+public interface SupplyService extends BaseService<Supply> {
+}
