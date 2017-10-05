ea101d3217fff223e743a5769aa39bffb0727438
Colvin Cooper <colvin.cooper@gmail.com>
2017-10-05 13:08:11 +0800

    Created Brand Repository Interface and Implementation

---
+++ repository/src/main/java/com/deped/repository/brand/BrandRepositoryImpl.java
@@ -0,0 +1,38 @@
+package com.deped.repository.brand;
+
+import com.deped.model.items.Brand;
+import com.deped.repository.utils.Range;
+
+import java.util.List;
+
+public class BrandRepositoryImpl implements BrandRepositoryInt {
+    @Override
+    public Brand create(Brand entity) {
+        return null;
+    }
+
+    @Override
+    public Boolean update(Brand entity) {
+        return null;
+    }
+
+    @Override
+    public List<Brand> fetchAll() {
+        return null;
+    }
+
+    @Override
+    public List<Brand> fetchByRange(Range range) {
+        return null;
+    }
+
+    @Override
+    public Brand fetchById(Object id) {
+        return null;
+    }
+
+    @Override
+    public Boolean remove(Brand... entities) {
+        return null;
+    }
+}
---
+++ repository/src/main/java/com/deped/repository/brand/BrandRepositoryInt.java
@@ -0,0 +1,7 @@
+package com.deped.repository.brand;
+
+import com.deped.model.items.Brand;
+import com.deped.repository.BaseRepository;
+
+public interface BrandRepositoryInt extends BaseRepository<Brand>{
+}
